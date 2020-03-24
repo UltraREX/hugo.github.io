0b21cd5b24f1d7ef8ace47db5b070ef6a34c89e2
Ray Young <ringxtee@gmail.com>
2020-03-24 16:33:20 +0800

    modify about

--- js/back_to_top.js
+++ js/back_to_top.js
@@ -1,17 +1,20 @@
 let bttCollection = this.document.getElementsByClassName("back-to-top");
 let btt = bttCollection[0];
-window.addEventListener(
-    'scroll',
-    function (e) {
-        let scroll_height = window.scrollY;
-        if (scroll_height > 315) {
-            if (btt.style.display == "none") {
-                btt.style.display = "";
-            }
-        } else {
-            if (btt.style.display != "none") {
-                btt.style.display = "none";
+if (btt)
+{
+    window.addEventListener(
+        'scroll',
+        function (e) {
+            let scroll_height = window.scrollY;
+            if (scroll_height > 315) {
+                if (btt.style.display == "none") {
+                    btt.style.display = "";
+                }
+            } else {
+                if (btt.style.display != "none") {
+                    btt.style.display = "none";
+                }
             }
         }
-    }
-);
+    );
+}
--- js/toc.js
+++ js/toc.js
@@ -1,20 +1,23 @@
 let toc = this.document.getElementById("post-toc");
-let fixedClsName = "post-toc-fixed";
-let absoluteClsName = "post-toc-absolute";
-window.addEventListener(
-    'scroll',
-    function (e) {
-        let scroll_height = window.scrollY;
-        if (scroll_height > 315) {
-            if (toc.className != fixedClsName)
-            {
-                toc.className = fixedClsName;
-            }
-        } else {
-            if (toc.className != absoluteClsName)
-            {
-                toc.className = absoluteClsName;
+if (toc)
+{
+    let fixedClsName = "post-toc-fixed";
+    let absoluteClsName = "post-toc-absolute";
+    window.addEventListener(
+        'scroll',
+        function (e) {
+            let scroll_height = window.scrollY;
+            if (scroll_height > 315) {
+                if (toc.className != fixedClsName)
+                {
+                    toc.className = fixedClsName;
+                }
+            } else {
+                if (toc.className != absoluteClsName)
+                {
+                    toc.className = absoluteClsName;
+                }
             }
         }
-    }
-);
+    );
+}
